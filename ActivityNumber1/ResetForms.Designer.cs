861cdb2654bc22f44daf5d58680f88a227a28d17
Starlapot@xvk.me <Starlapot@xvk.me>
2023-09-29 11:58:05 +0800

    ResetForms design done ;)

--- ActivityNumber1/ResetForms.Designer.cs
+++ ActivityNumber1/ResetForms.Designer.cs
@@ -28,24 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ResetForms));
             this.Changebtn = new System.Windows.Forms.Button();
             this.passwordtextBox = new System.Windows.Forms.TextBox();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
             this.newpasslbl = new System.Windows.Forms.Label();
-            this.closeBtn = new System.Windows.Forms.PictureBox();
-            this.showBtn = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.newpasstextBox = new System.Windows.Forms.TextBox();
-            this.showBtn1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
             this.closeBtn1 = new System.Windows.Forms.PictureBox();
+            this.closeBtn = new System.Windows.Forms.PictureBox();
+            this.showBtn = new System.Windows.Forms.PictureBox();
+            this.showBtn1 = new System.Windows.Forms.PictureBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.BackBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.showBtn)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.showBtn1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.closeBtn1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // Changebtn
@@ -53,9 +56,9 @@
             this.Changebtn.BackColor = System.Drawing.Color.Black;
             this.Changebtn.Font = new System.Drawing.Font("Simple Handmade", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Changebtn.ForeColor = System.Drawing.Color.White;
-            this.Changebtn.Location = new System.Drawing.Point(173, 288);
+            this.Changebtn.Location = new System.Drawing.Point(170, 250);
             this.Changebtn.Name = "Changebtn";
-            this.Changebtn.Size = new System.Drawing.Size(97, 34);
+            this.Changebtn.Size = new System.Drawing.Size(80, 34);
             this.Changebtn.TabIndex = 0;
             this.Changebtn.Text = "Change";
             this.Changebtn.UseVisualStyleBackColor = false;
@@ -63,8 +66,9 @@
             // 
             // passwordtextBox
             // 
+            this.passwordtextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(154)))), ((int)(((byte)(122)))));
             this.passwordtextBox.Font = new System.Drawing.Font("Simple Handmade", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.passwordtextBox.Location = new System.Drawing.Point(173, 209);
+            this.passwordtextBox.Location = new System.Drawing.Point(170, 171);
             this.passwordtextBox.Multiline = true;
             this.passwordtextBox.Name = "passwordtextBox";
             this.passwordtextBox.PasswordChar = '*';
@@ -75,9 +79,10 @@
             // 
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.BackColor = System.Drawing.Color.White;
-            this.linkLabel1.Location = new System.Drawing.Point(276, 288);
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel1.Location = new System.Drawing.Point(158, 100);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(100, 13);
+            this.linkLabel1.Size = new System.Drawing.Size(145, 18);
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "common passwords";
@@ -88,17 +93,90 @@
             this.newpasslbl.AutoSize = true;
             this.newpasslbl.BackColor = System.Drawing.Color.White;
             this.newpasslbl.Font = new System.Drawing.Font("Simple Handmade", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newpasslbl.Location = new System.Drawing.Point(37, 209);
+            this.newpasslbl.Location = new System.Drawing.Point(36, 171);
             this.newpasslbl.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.newpasslbl.Name = "newpasslbl";
             this.newpasslbl.Size = new System.Drawing.Size(131, 25);
             this.newpasslbl.TabIndex = 3;
             this.newpasslbl.Text = "New Password";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Simple Handmade", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 218);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 25);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Re-enter Password";
+            // 
+            // newpasstextBox
+            // 
+            this.newpasstextBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(154)))), ((int)(((byte)(122)))));
+            this.newpasstextBox.Font = new System.Drawing.Font("Simple Handmade", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.newpasstextBox.Location = new System.Drawing.Point(170, 212);
+            this.newpasstextBox.Multiline = true;
+            this.newpasstextBox.Name = "newpasstextBox";
+            this.newpasstextBox.PasswordChar = '*';
+            this.newpasstextBox.Size = new System.Drawing.Size(170, 32);
+            this.newpasstextBox.TabIndex = 15;
+            this.newpasstextBox.TextChanged += new System.EventHandler(this.newpasstextBox_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Simple Handmade", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(8, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(226, 31);
+            this.textBox1.TabIndex = 19;
+            this.textBox1.Text = "Change Password\r\n";
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Simple Handmade", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(8, 35);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(371, 133);
+            this.textBox2.TabIndex = 20;
+            this.textBox2.Text = resources.GetString("textBox2.Text");
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.BackColor = System.Drawing.Color.White;
+            this.linkLabel2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabel2.Location = new System.Drawing.Point(160, 121);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(136, 18);
+            this.linkLabel2.TabIndex = 21;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "Have I Been Pwned";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // closeBtn1
+            // 
+            this.closeBtn1.BackColor = System.Drawing.Color.White;
+            this.closeBtn1.Image = global::ActivityNumber1.Properties.Resources.ClosePassword;
+            this.closeBtn1.Location = new System.Drawing.Point(342, 213);
+            this.closeBtn1.Margin = new System.Windows.Forms.Padding(2);
+            this.closeBtn1.Name = "closeBtn1";
+            this.closeBtn1.Size = new System.Drawing.Size(42, 32);
+            this.closeBtn1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.closeBtn1.TabIndex = 17;
+            this.closeBtn1.TabStop = false;
+            this.closeBtn1.Visible = false;
+            this.closeBtn1.Click += new System.EventHandler(this.closeBtn1_Click);
+            // 
             // closeBtn
             // 
+            this.closeBtn.BackColor = System.Drawing.Color.White;
             this.closeBtn.Image = global::ActivityNumber1.Properties.Resources.ClosePassword;
-            this.closeBtn.Location = new System.Drawing.Point(343, 209);
+            this.closeBtn.Location = new System.Drawing.Point(342, 173);
             this.closeBtn.Margin = new System.Windows.Forms.Padding(2);
             this.closeBtn.Name = "closeBtn";
             this.closeBtn.Size = new System.Drawing.Size(42, 32);
@@ -110,8 +188,9 @@
             // 
             // showBtn
             // 
+            this.showBtn.BackColor = System.Drawing.Color.White;
             this.showBtn.Image = global::ActivityNumber1.Properties.Resources.ShowPassword;
-            this.showBtn.Location = new System.Drawing.Point(343, 209);
+            this.showBtn.Location = new System.Drawing.Point(342, 171);
             this.showBtn.Margin = new System.Windows.Forms.Padding(2);
             this.showBtn.Name = "showBtn";
             this.showBtn.Size = new System.Drawing.Size(42, 32);
@@ -120,33 +199,11 @@
             this.showBtn.TabStop = false;
             this.showBtn.Click += new System.EventHandler(this.showBtn_Click);
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.White;
-            this.label1.Font = new System.Drawing.Font("Simple Handmade", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 256);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 25);
-            this.label1.TabIndex = 14;
-            this.label1.Text = "Re-enter Password";
-            // 
-            // newpasstextBox
-            // 
-            this.newpasstextBox.Font = new System.Drawing.Font("Simple Handmade", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.newpasstextBox.Location = new System.Drawing.Point(173, 250);
-            this.newpasstextBox.Multiline = true;
-            this.newpasstextBox.Name = "newpasstextBox";
-            this.newpasstextBox.PasswordChar = '*';
-            this.newpasstextBox.Size = new System.Drawing.Size(170, 32);
-            this.newpasstextBox.TabIndex = 15;
-            this.newpasstextBox.TextChanged += new System.EventHandler(this.newpasstextBox_TextChanged);
-            // 
             // showBtn1
             // 
+            this.showBtn1.BackColor = System.Drawing.Color.White;
             this.showBtn1.Image = global::ActivityNumber1.Properties.Resources.ShowPassword;
-            this.showBtn1.Location = new System.Drawing.Point(343, 250);
+            this.showBtn1.Location = new System.Drawing.Point(342, 212);
             this.showBtn1.Margin = new System.Windows.Forms.Padding(2);
             this.showBtn1.Name = "showBtn1";
             this.showBtn1.Size = new System.Drawing.Size(42, 32);
@@ -155,48 +212,44 @@
             this.showBtn1.TabStop = false;
             this.showBtn1.Click += new System.EventHandler(this.showBtn1_Click);
             // 
-            // closeBtn1
-            // 
-            this.closeBtn1.Image = global::ActivityNumber1.Properties.Resources.ClosePassword;
-            this.closeBtn1.Location = new System.Drawing.Point(343, 250);
-            this.closeBtn1.Margin = new System.Windows.Forms.Padding(2);
-            this.closeBtn1.Name = "closeBtn1";
-            this.closeBtn1.Size = new System.Drawing.Size(42, 32);
-            this.closeBtn1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.closeBtn1.TabIndex = 17;
-            this.closeBtn1.TabStop = false;
-            this.closeBtn1.Visible = false;
-            this.closeBtn1.Click += new System.EventHandler(this.closeBtn1_Click);
-            // 
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.Color.White;
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(393, 333);
+            this.pictureBox1.Size = new System.Drawing.Size(393, 298);
             this.pictureBox1.TabIndex = 18;
             this.pictureBox1.TabStop = false;
             // 
-            // pictureBox2
+            // BackBtn
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(390, 200);
-            this.pictureBox2.TabIndex = 19;
-            this.pictureBox2.TabStop = false;
+            this.BackBtn.BackColor = System.Drawing.Color.Black;
+            this.BackBtn.Font = new System.Drawing.Font("Simple Handmade", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BackBtn.ForeColor = System.Drawing.Color.White;
+            this.BackBtn.Location = new System.Drawing.Point(260, 249);
+            this.BackBtn.Margin = new System.Windows.Forms.Padding(2);
+            this.BackBtn.Name = "BackBtn";
+            this.BackBtn.Size = new System.Drawing.Size(80, 34);
+            this.BackBtn.TabIndex = 22;
+            this.BackBtn.Text = "Back";
+            this.BackBtn.UseVisualStyleBackColor = false;
+            this.BackBtn.Click += new System.EventHandler(this.BackBtn_Click);
             // 
             // ResetForms
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(391, 328);
-            this.Controls.Add(this.pictureBox2);
+            this.ClientSize = new System.Drawing.Size(389, 293);
+            this.Controls.Add(this.BackBtn);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.closeBtn1);
             this.Controls.Add(this.newpasstextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.closeBtn);
             this.Controls.Add(this.newpasslbl);
-            this.Controls.Add(this.linkLabel1);
             this.Controls.Add(this.passwordtextBox);
             this.Controls.Add(this.Changebtn);
             this.Controls.Add(this.showBtn);
@@ -206,12 +259,11 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Password Reset";
             this.Load += new System.EventHandler(this.ResetForms_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.closeBtn1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.closeBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.showBtn)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.showBtn1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.closeBtn1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -230,6 +282,9 @@
         private System.Windows.Forms.PictureBox showBtn1;
         private System.Windows.Forms.PictureBox closeBtn1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button BackBtn;
     }
 }
